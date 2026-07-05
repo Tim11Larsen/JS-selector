0461276e355a71e23f9ba25aa4d9a3fe11af1d9f
Timothy Larsen <x98larsen@yahoo.com>
2026-07-05 16:17:36 -0400

    HTML

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 //http://www.omdbapi.com/?i=tt3896198&apikey=71c4119b
-const data = (fetch('http://www.omdbapi.com/?i=tt3896198&apikey=71c4119b'))
-console.log(data)
+// const data = (fetch('http://www.omdbapi.com/?i=tt3896198&apikey=71c4119b'))
+// console.log(data)
+
+async function renderMovies() {
+    const movies = await fetch('http://www.omdbapi.com/?i=tt3896198&apikey=71c4119b')
+    const moviesData = await movies.json();
+    const movieListEl = document.querySelector(".movie-list");
+
+    userListEl.innerHTML = moviesData.map((movie) => )
+}
+console.log(renderMovies())
